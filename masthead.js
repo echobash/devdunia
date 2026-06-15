b134537600b08854a10de78e0fc700a5b4524b8c
Ali Anwar <greenywish@gmail.com>
2026-06-15 23:10:10 +0530

    Refactor the base64

--- masthead.js
+++ masthead.js
@@ -119,10 +119,10 @@
     el.className = 'dd-masthead';
     el.innerHTML =
       '<div class="dd-mast-left">' +
-        '<a href="../index.html">' +
+        '<a href="index.html">' +
           '<img src="images/logo.png" alt="DevDunia">' +
         '</a>' +
-        '<a href="../index.html" class="dd-brand">DEV<span>DUNIA</span> COMICS</a>' +
+        '<a href="index.html" class="dd-brand">DEV<span>DUNIA</span> COMICS</a>' +
       '</div>' +
       '<div style="text-align:center">' +
         '<span class="dd-issue-pill">DEVDUNIA &middot; JUNE 2026</span>' +
@@ -136,7 +136,7 @@
         '<a href="https://github.com/echobash" target="_blank" rel="noopener" class="dd-icon-btn" title="GitHub">' +
           '<svg viewBox="0 0 24 24" fill="#1a1410"><path d="M12 0C5.37 0 0 5.37 0 12c0 5.3 3.44 9.8 8.2 11.38.6.1.82-.26.82-.58v-2.03c-3.34.72-4.04-1.61-4.04-1.61-.54-1.38-1.33-1.75-1.33-1.75-1.09-.74.08-.73.08-.73 1.2.08 1.84 1.24 1.84 1.24 1.07 1.83 2.8 1.3 3.49 1 .1-.78.42-1.3.76-1.6-2.67-.3-5.47-1.33-5.47-5.93 0-1.31.47-2.38 1.24-3.22-.13-.3-.54-1.52.12-3.18 0 0 1.01-.32 3.3 1.23a11.5 11.5 0 013-.4c1.02 0 2.04.13 3 .4 2.28-1.55 3.29-1.23 3.29-1.23.66 1.66.25 2.88.12 3.18.77.84 1.24 1.91 1.24 3.22 0 4.61-2.81 5.63-5.48 5.92.43.37.81 1.1.81 2.22v3.29c0 .32.22.69.83.57C20.57 21.8 24 17.3 24 12c0-6.63-5.37-12-12-12z"/></svg>' +
         '</a>' +
-        '<a href="../index.html" class="dd-back-link">&#9664; ALL TOOLS</a>' +
+        '<a href="index.html" class="dd-back-link">&#9664; ALL TOOLS</a>' +
       '</div>';
   }
 
